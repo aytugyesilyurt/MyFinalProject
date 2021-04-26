2e9b3d41bd8a39cae5c0e5c5cbb4092597937e36
Aytug <80128606+aytugyesilyurt@users.noreply.github.com>
2021-04-26 04:40:45 +0300

    entityFramework giriş

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -6,16 +6,10 @@ using System.Text;
 namespace DataAccess.Abstract
 {
     //Product ile ilgili veritabanında yapılacak işlemleri içeren interface
-    //Entitiesdeki productı kullanmak için katmanı referans vermek gerekir.(sağ tuş add to referance)
+    //Entitiesdeki productı kullanmak için katmanı referans vermek gerekir.(sağ tuş add to project referance)
     public interface IProductDal
     {
-        List<Product> GetAll();
-        void Add(Product product);
-        void Update(Product product);
-        void Delete(Product product);
-
-        // ürünleri kategoriye göre listele
-        List<Product> GetAllByCategory(int categoryId);
+        
 
     }
 }
