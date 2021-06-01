b9ca54df8b8d8597ae43eeb0bc556b140dbd8e52
Aytug <yesilyurt.aytug1@gmail.com>
2021-06-01 16:26:50 +0300

    w10/part1

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        //constructor class çalıştığı zaman ayrıca this(Result) yazarak aynı zamanda diğer constructor bloğunun da çalışmasını sağlar.
+        public Result(bool success, string message):this(success)
+        {
+            Message = message;
+        }
+
+        // overload yaparak mesaj yazmadan sadece true false olarak da döndürebiliriz yada sadece mesaj olarak da yazılabilirdi
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
